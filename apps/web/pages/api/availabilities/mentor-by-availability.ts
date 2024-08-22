19e272cb2b57961b2972067fcd9d2ae48f85436a
Danial Yusra <daniyusra@gmail.com>
2024-08-22 21:29:50 +0900

    incorporate existing api to create better results

--- apps/web/pages/api/availabilities/mentor-by-availability.ts
+++ apps/web/pages/api/availabilities/mentor-by-availability.ts
@@ -1,13 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 
+import { getUsersAvailability } from "@calcom/core/getUserAvailability";
 import prisma from "@calcom/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const getMentorsSchema = z
     .object({
-      startTime: z.date().or(z.string()).optional(),
-      endTime: z.date().or(z.string()).optional(),
+      startTime: z.date().or(z.string()),
+      endTime: z.date().or(z.string()),
       take: z.number().optional(),
       skip: z.number().optional(),
     })
@@ -60,23 +61,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         id: true,
         name: true,
         email: true,
-        schedules: {
-          select: {
-            availability: {
-              select: {
-                startTime: true,
-                endTime: true,
-              },
-            },
-          },
-        },
       },
       take,
       skip,
     });
 
+    console.log(selectedUsers);
+
+    const allUsersAvailability = (
+      await getUsersAvailability({
+        users: selectedUsers,
+        query: {
+          dateFrom: startTime,
+          dateTo: endTime,
+          returnDateOverrides: true,
+        },
+      })
+    ).map(({ busy, dateRanges, oooExcludedDateRanges, timeZone, datesOutOfOffice }, index) => {
+      const currentUser = selectedUsers[index];
+      return {
+        timeZone,
+        dateRanges,
+        oooExcludedDateRanges,
+        busy,
+        user: currentUser,
+        datesOutOfOffice,
+      };
+    });
+
     res.status(200).json({
-      mentors: selectedUsers,
+      mentors: allUsersAvailability,
       message: "Get Mentors by Availability Successful",
     });
   }
